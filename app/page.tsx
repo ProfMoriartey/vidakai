b4e7d378d830ecc3a7fa6ed6fc7fbb802c4188b2
ProfMoriartey <61118848+ProfMoriartey@users.noreply.github.com>
2025-02-20 10:07:39 +0300

    Remove Image import, add animated dots

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,37 @@
-import Image from "next/image";
+// import Image from "next/image";
+"use client"
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [dots, setDots] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots(prev => prev.length < 3 ? prev + '.' : '');
+    }, 500); // Change dot every 500ms
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="font-[family-name:var(--font-geist-mono)] text-4xl">
-          Coming Soon...
+          Coming Soon<span className="inline-block font-mono w-8 text-left ml-3">
+          {dots}
+        </span>
         </h1>
       </main>
       
     </div>
   );
 }
+
+
+
+
+  
+
+
 
 {/* <div>
   <Image
